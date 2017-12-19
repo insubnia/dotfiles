58529bb9955f6c1b1bd3aaf5d07cf1ee893a7d7c
sis <sis06232@gmail.com>
2017-12-20 02:51:24 +0900

    config > add alias and supplement the job indicator function (in progress)

--- config_bash.sh
+++ config_bash.sh
@@ -13,38 +13,33 @@ while read line; do
     fi
 done < $BASHRC
 
-
 echo $TAG_FLAG
 
 
-
-
-# use $(job_indicator)\$ $(tput sgr0) 
-# function job_indicator() {
-#     if [ -n "$(jobs)" ]; then
-#         echo "$(tput setaf 1)"
-#     fi
-# }
-# alias python='usr/bin/python3'
-# alias grep='grep --color=auto --exclude={tags,*.lst,*.map,*.d} --exclude-dir={.git,.svn}'
-# alias ssh='ssh -X'
-
-function job_indicator() {
-    if [ -n "$(jobs)" ]; then
-        echo "$(tput setaf 1)"
-    fi
-}
-
 # Append custom configuration, when there is no tag
-# if [ $TAG_FLAG -eq 0 ]; then
-if [ $TAG_FLAG -eq 1 ]; then
+if [ $TAG_FLAG -eq 0 ]; then
+    echo $MY_TAG
+    FIND="\$ "
+    REPLACE="\$(job_indicator)\\\$\$(tput sgr0) "
+    RESULT=${PS1//$FIND/$REPLACE}
 
     echo $PS1
-    FIND="\$ "
-    REPLACE="\$(job_indicator)\$\$(tput sgr0) "
-    RESULT=${PS1//$FIND/$REPLACE}
-    echo $RESULT
-
-    # echo $RESULT >> $BASHRC
+    echo PS1=\'$RESULT\'
 fi
+# fi >> $BASHRC
+
+while read line; do
+    echo $line
+done < <(cat << EOF
+    alias python='usr/bin/python3'
+    alias grep='grep --color=auto --exclude={tags,*.lst,*.map,*.d} --exclude-dir={.git,.svn}'
+    alias ssh='ssh -X'
+    function job_indicator() {
+        if [ -n "\$(jobs)" ]; then
+            echo "\$(tput setaf 1)"
+        fi
+    }
+EOF
+)
+
 
