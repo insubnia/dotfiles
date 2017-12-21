af37ec921ed7e6ee247034b228edcffa67d47205
sis <sis06232@gmail.com>
2017-12-21 18:07:24 +0900

    config > remove bashrc file & complete config_bash.sh (w/o remove option)

--- config_bash.sh
+++ config_bash.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
 BASHRC=~/.bashrc
-
 MY_TAG="# sis bashrc"
 TAG_FLAG=0
 
 chmod a+rwx $BASHRC
 
-# echo MODE
-# echo 1. Append configuration
-# echo 2. Rmove configuration
-# read -p "Select mode: " mode
-# echo
-
-if (grep -q "$MY_TAG" $BASHRC); then TAG_FLAG=1; fi
-echo $TAG_FLAG
-
-while read line; do
-    echo $line
-done < <(cat << EOF
-
-    $MY_TAG
-    alias python='usr/bin/python3'
-    alias grep='grep --color=auto --exclude={tags,*.lst,*.map,*.d} --exclude-dir={.git,.svn}'
-    alias ssh='ssh -X'
-
-    function job_indicator() {
-        if [ -n "\$(jobs)" ]; then
-            echo "\$(tput setaf 1)"
-        fi
-    }
-EOF
-# ) >> $BASHRC
-)
-
-# Append custom configuration, when there is no tag
-if [ $TAG_FLAG -eq 0 ]; then
+function get_ps1() {
     FIND='\\\$ '
     REPLACE='$(job_indicator)\$$(tput sgr0) '
     RESULT=${PS1//$FIND/$REPLACE}
-
     echo "PS1='$RESULT'"
-# fi >> $BASHRC
-fi
+}
+
+my_config=$(cat << EOF
+$MY_TAG
+export DISPLAY=:0
+
+alias python='python3'
+alias grep='grep --color=auto --exclude={tags,*.lst,*.map,*.d} --exclude-dir={.git,.svn}'
+alias ssh='ssh -X'
+
+function job_indicator() { if [ -n "\$(jobs)" ]; then echo "\$(tput setaf 1)"; fi }
+
+$(get_ps1)
+EOF
+)
+
+function clear_config() {
+    # Delete below $MYTAG
+    source $BASHRC
+}
+
+function set_config() {
+    if [ $TAG_FLAG -ne 1 ]; then
+        echo "$my_config" >> $BASHRC
+    fi
+}
+
+echo MODE
+echo 1. Append configuration
+echo 2. Rmove configuration
+echo 3. Do nothing
+read -p "Select mode: " mode
+echo
+
+if (grep -q "$MY_TAG" $BASHRC); then TAG_FLAG=1; fi
+
+case "$mode" in
+    1)
+        $(clear_config)
+        $(set_config)
+        ;;
+    2)
+        $(clear_config)
+        ;;
+    *)
+        echo Exit
+        ;;
+esac
 
