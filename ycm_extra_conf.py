af054aa3c85bfab9bc45fcea426a3889d8a9bf07
sis <sis06232@gmail.com>
2018-03-18 02:37:37 +0900

    sh > enable ycm script to add all sub directories with exception

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -1,4 +1,16 @@
-def FlagsForFile( filename, **kwargs ):
+import os
+
+def FlagsForFile(filename, **kwargs):
+    inc_dirs = []
+    for i,j,k in os.walk("."):
+        if ".git" in i:
+            pass
+        else:
+            inc_dirs += ["-I" + i]
+
+    flags = ["-W", "-Wall"]
+    flags.extend(inc_dirs)
+
     return {
-            'flags': ['-W','-Wall'],
+            'flags': flags,
             }
