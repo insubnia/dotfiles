e45dd60a075ec2a3cd54683c74701d3229ebda83
sis <sis06232@gmail.com>
2018-05-21 17:14:28 +0900

    script > add startup directory

--- zshrc.sh
+++ zshrc.sh
@@ -1,4 +1,9 @@
 # sis zshrc
+if [ $PWD = $HOME ]; then
+    mkdir -p workspace
+    cd workspace
+fi
+
 ZSH_THEME="agnoster"
 plugins=(
     colored-man-pages
@@ -11,8 +16,6 @@ plugins=(
 )
 source $ZSH/oh-my-zsh.sh
 unsetopt BG_NICE
-
-export DISPLAY=:0
 
 alias python='python3'
 alias grep='grep --color=auto --exclude={tags} --exclude-dir={.git,.svn}'
