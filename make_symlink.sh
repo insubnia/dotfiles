3c7c6dbcce87f31af3cd8bd6a1e3c8bbecbf8dbd
sis <sis06232@gmail.com>
2018-10-11 17:05:09 +0900

    tweak scripts

--- make_symlink.sh
+++ make_symlink.sh
@@ -4,7 +4,7 @@ DOTFILES=$HOME/workspace/dotfiles
 
 TAR=$HOME/.ctags
 if [ ! -e $TAR ]; then
-    ln -s $DOTFILES/ctags $TAR
+    ln -sf $DOTFILES/ctags $TAR
     echo Make exuberant-ctags symlink
 else
     echo .ctags already exists
@@ -13,7 +13,7 @@ fi
 TAR=$HOME/.ctags.d/default.ctags
 if [ ! -e $TAR ]; then
     mkdir -p $(dirname $TAR)
-    ln -s $DOTFILES/ctags $TAR
+    ln -sf $DOTFILES/ctags $TAR
     echo Make universal-ctags symlink
 else
     echo default.ctags already exists
@@ -21,7 +21,7 @@ fi
 
 TAR=$HOME/.ackrc
 if [ ! -e $TAR ]; then
-    ln -s $DOTFILES/ackrc $TAR
+    ln -sf $DOTFILES/ackrc $TAR
     echo Make ackrc symlink
 else
     echo .ackrc already exists
