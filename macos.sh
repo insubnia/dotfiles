d0aabe58233768ae3e4bd3aa24c94e37905b9fcd
sis <sis06232@gmail.com>
2018-10-23 00:09:52 +0900

    script > tweak macos.sh

--- macos.sh
+++ macos.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-# installation with homebrew
+
+# At first, install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 while read line
 do
-    echo -e "\n[Installing $line]"
-    # brew cask install
-    echo $line
+    brew cask install $line
 done < <(cat << EOF
     iterm2
     sourcetree
-    font-hack-nerd-font
+    karabiner-elements
+    appcleaner
+    flash-npapi
+    vlc
     folx
     mounty
 EOF
 )
 
-echo -e "\n\nInstallation complete!!\n"
+brew tap homebrew/cask-fonts
+while read line
+do
+    brew cask install $line
+done < <(cat << EOF
+    font-d2coding
+    font-hack-nerd-font
+EOF
+)
+
+echo -e "\n\nInstallation complete\n\n"
