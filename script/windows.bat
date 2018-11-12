3fe108e3e87d9af83d3661fa7ca0d34bdf55a914
sis <sis06232@gmail.com>
2018-11-12 23:21:44 +0900

    script > add vim after symbolic link on windows.bat

--- script/windows.bat
+++ script/windows.bat
@@ -2,3 +2,4 @@ set DOTFILES=%USERPROFILE%/workspace/dotfiles
 
 mklink "%USERPROFILE%/_vimrc"    "%DOTFILES%/vimrc"
 mklink "%USERPROFILE%/.pylintrc" "%DOTFILES%/conf/pylintrc"
+mklink /d "%USERPROFILE%/.vim/after" "%DOTFILES%/conf/vim-after"
