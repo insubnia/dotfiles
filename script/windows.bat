78c4560f19c704e6ebf874c36527b6024b09ee72
sis <sis06232@gmail.com>
2018-11-21 17:19:54 +0900

    update windows.bar

--- script/windows.bat
+++ script/windows.bat
@@ -5,3 +5,11 @@ mklink "%USERPROFILE%/.ackrc"    "%DOTFILES%/conf/ackrc"
 mklink "%USERPROFILE%/.pylintrc" "%DOTFILES%/conf/pylintrc"
 
 mklink /d "%USERPROFILE%/vimfiles/after" "%DOTFILES%/conf/vim-after"
+
+
+choco install -y wget
+choco install -y vim-tux
+choco install -y python3
+choco install -y sourcetree
+choco install -y cmder
+choco install -y babun
