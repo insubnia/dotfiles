fcd91bf9c90acc69a8c5a7505a8a9cf0e27258e3
sis <sis06232@gmail.com>
2019-01-02 00:10:08 +0900

    update script

--- script/windows.bat
+++ script/windows.bat
@@ -7,7 +7,9 @@ mklink "%USERPROFILE%/.flake8" "%DOTFILES%/conf/flake8"
 REM  tool
 choco install -y chocolateygui
 choco install -y googlechrome
+choco install -y onedrive
 choco install -y flashplayerplugin
+choco install -y vlc
 choco install -y vim-tux
 choco install -y sourcetree
 choco install -y vscode
