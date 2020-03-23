b26adadaff56b8fb660a9eef328729440292cb2c
sis <sis06232@gmail.com>
2020-03-23 12:08:33 +0900

    update windows settings

---
+++ script/windows-python.bat
@@ -0,0 +1,6 @@
+python -m pip install flake8
+python -m pip install pep8-naming
+python -m pip install autopep8
+python -m pip install pyqt5
+python -m pip install python-language-server
+python -m pip install pynvim
--- script/windows.bat
+++ script/windows.bat
@@ -5,6 +5,10 @@ mklink "%USERPROFILE%/.ackrc" "%DOTFILES%/conf/ackrc"
 mklink "%USERPROFILE%/.flake8" "%DOTFILES%/conf/flake8"
 REM  mklink /D "%USERPROFILE%/.vim" "%USERPROFILE%/vimfiles"
 
+mkdir "%USERPROFILE%/AppData/Local/nvim"
+mklink "%USERPROFILE%/AppData/Local/nvim/init.vim" "%DOTFILES%/vimrc"
+mklink "%USERPROFILE%/AppData/Local/nvim/coc-settings.json" "%DOTFILES%/conf/coc-settings.json"
+
 REM  tool
 choco install -y chocolateygui
 choco install -y googlechrome
