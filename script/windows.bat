27df8ea72662493723b4b0fd68d8e0ca1133c636
sis <sis06232@gmail.com>
2020-08-27 12:49:55 +0900

    Update windows settings
    1. Separate batch file
    2. Add vs code settings.json

--- script/windows.bat
+++ script/windows.bat
@@ -1,14 +1,3 @@
-set DOTFILES=%USERPROFILE%/workspace/dotfiles
-
-mklink "%USERPROFILE%/_vimrc" "%DOTFILES%/vimrc"
-mklink "%USERPROFILE%/.ackrc" "%DOTFILES%/conf/ackrc"
-mklink "%USERPROFILE%/.flake8" "%DOTFILES%/conf/flake8"
-REM  mklink /D "%USERPROFILE%/.vim" "%USERPROFILE%/vimfiles"
-
-mkdir "%USERPROFILE%/AppData/Local/nvim"
-mklink "%USERPROFILE%/AppData/Local/nvim/init.vim" "%DOTFILES%/vimrc"
-mklink "%USERPROFILE%/AppData/Local/nvim/ginit.vim" "%DOTFILES%/vim/ginit.vim"
-
 REM  tool
 choco install -y chocolateygui
 choco install -y googlechrome
