dc6da18577e3492a377c20736d95ef8737d1ee3c
sis <sis06232@gmail.com>
2020-12-11 01:30:17 +0900

    organize script directory

--- script/4_set_symlink.sh
+++ script/4_set_symlink.sh
@@ -4,18 +4,7 @@ DOTFILES=~/workspace/dotfiles
 CONF=$DOTFILES/conf
 VSCODE=$DOTFILES/vscode
 
-# TAR=~/.ctags.d/default.ctags
-# if [ ! -e $TAR ]; then
-#     mkdir -p $(dirname $TAR)
-#     ln -sf $CONF/ctags $TAR
-#     echo Make universal-ctags symlink
-# else
-#     echo default.ctags already exists
-# fi
-
 ln -sf $CONF/ackrc ~/.ackrc
-ln -sf $CONF/tigrc ~/.tigrc
-ln -sf $CONF/ctags ~/.ctags
 ln -sf $CONF/flake8 ~/.flake8
 
 case "$OSTYPE" in
