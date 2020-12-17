5e4774b9c09328e51e354bdc00e6d31b2e81afad
sis <sis06232@gmail.com>
2020-12-17 12:48:38 +0900

    update windows install list

--- script/windows-install.bat
+++ script/windows-install.bat
@@ -1,4 +1,4 @@
-REM  tool
+:: tool
 choco install -y chocolateygui
 choco install -y googlechrome
 choco install -y onedrive
@@ -10,12 +10,14 @@ choco install -y sourcetree
 choco install -y vscode
 choco install -y tortoisegit
 
-REM server
+:: server-client
 choco install -y openssh
 choco install -y putty
 choco install -y winscp
+choco install -y telnet
+choco install -y teraterm
 
-REM  dev
+:: dev
 choco install -y git
 choco install -y ack
 choco install -y llvm
@@ -31,5 +33,5 @@ choco install -y dos2unix
 choco install -y microsoft-windows-terminal
 choco install -y vcxsrv
 
-REM  font
+:: font
 choco install -y d2codingfont
