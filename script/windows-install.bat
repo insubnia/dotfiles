dd5634c6b66d099988c1ef637f622a5ae2bc0ba7
sis <sis06232@gmail.com>
2021-01-08 10:19:50 +0900

    add foxe to windows-install list

--- script/windows-install.bat
+++ script/windows-install.bat
@@ -2,16 +2,17 @@
 choco install -y chocolateygui
 choco install -y googlechrome
 choco install -y onedrive
-REM  choco install -y flashplayerplugin
 choco install -y vlc
 choco install -y vim
 choco install -y neovim
+choco install -y foxe
 choco install -y sourcetree
 choco install -y vscode
 choco install -y tortoisegit
 choco install -y caffeine
 choco install -y winpcap
 choco install -y wireshark
+REM  choco install -y flashplayerplugin
 
 :: server-client
 choco install -y openssh
@@ -34,7 +35,7 @@ choco install -y jq
 choco install -y wget
 choco install -y dos2unix
 choco install -y microsoft-windows-terminal
-choco install -y vcxsrv
+REM  choco install -y vcxsrv
 
 :: font
 choco install -y d2codingfont
