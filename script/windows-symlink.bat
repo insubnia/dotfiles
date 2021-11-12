04a4fb858cf82bf88937fc0f36ddbd81b7156338
Insub Song <insub.song@halla.com>
2021-11-12 16:02:53 +0900

    update windows symlink

--- script/windows-symlink.bat
+++ script/windows-symlink.bat
@@ -10,6 +10,7 @@ del /f _vimrc .ackrc .flake8
 mklink "%USERPROFILE%/_vimrc" "%DOTFILES%/vimrc"
 mklink "%USERPROFILE%/.ackrc" "%DOTFILES%/conf/ackrc"
 mklink "%USERPROFILE%/.flake8" "%DOTFILES%/conf/flake8"
+mklink "%USERPROFILE%/.gitignore" "%DOTFILES%/conf/gitignore"
 ::mklink "%USERPROFILE%/Desktop/ip-shift.bat" "%DOTFILES%/script/windows-ip-shift.bat"
 
 
