8e47b682ca9c4035fc0d0c95aa0805dcd9b761a2
Insub Song <sis06232@gmail.com>
2022-03-31 02:49:40 +0900

    update mac install list

--- script/mac-install.sh
+++ script/mac-install.sh
@@ -20,10 +20,19 @@ done << EOF
     sourcetree
     visual-studio-code
     wireshark
+    mas
 EOF
 
-mas install 869223134  # kakaotalk
-mas install 441258766  # magnet
+# https://github.com/mas-cli/mas
+mas install 869223134  # KakaoTalk
+mas install 1236050766 # Melon
+mas install 441258766  # Magnet
+mas install 1471801525 # polyglot
+mas install 1231935892 # Unicorn
+mas install 1475628500 # Unicorn HTTPS
+mas install 1033453958 # AllDic
+mas install 1594059167 # NflxMultiSubs
+mas install 1153157709 # Speedtest
 
 # Install fonts
 brew tap homebrew/cask-fonts
