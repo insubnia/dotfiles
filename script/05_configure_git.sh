2ac1491effd76658a20832340c6f5deabe430bd2
sis <sis06232@gmail.com>
2022-08-05 10:16:02 +0900

    update git config script

--- script/05_configure_git.sh
+++ script/05_configure_git.sh
@@ -10,14 +10,15 @@ git config --global core.safecrlf false
 git config --global core.excludesfile "~/workspace/dotfiles/conf/gitignore"
 git config --global core.ignorecase false
 
+git config --global rebase.false
+
+git config --global credential.helper store
+
 git config --global diff.tool vimdiff
 git config --global difftool.prompt true
-
 git config --global merge.tool vimdiff2     # tool: vimdiff, vimdiff2, vimdiff3
 git config --global mergetool.prompt true
 git config --global merge.conflictstyle diff3   # confilctstyle: merge, diff3
-
-git config --global rebase.false
 
 git config --global alias.cp      "cherry-pick"
 git config --global alias.df      "diff --color-words"
