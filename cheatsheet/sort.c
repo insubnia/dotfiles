c7e570d2aab994b4a9b8b0908fc6f7550b4c6de5
sis <sis06232@gmail.com>
2022-08-07 00:27:30 +0900

    add sort.c to cheatsheet

---
+++ cheatsheet/sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#define ARRAY_SIZE(x) (sizeof((x))/sizeof((x)[0])) 
+
+#define DTYPE uint16_t
+
+DTYPE data[] = { 17, 3, 5, 11, 9, 1 };
+
+void print_arr(DTYPE *arr, size_t n)
+{
+    printf("arr: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertion_sort(DTYPE *in, DTYPE *out, size_t n)
+{
+    memcpy(out, in, sizeof(DTYPE) * n);
+
+    for (int j, i = 1; i < n; i++) {
+        DTYPE val = out[(j=i)];
+        while (--j >= 0 && val < out[j])
+            out[j + 1] = out[j];
+        out[j + 1] = val;
+    }
+}
+
+int main(void)
+{
+    DTYPE out[ARRAY_SIZE(data)] = {};
+
+    printf("Before ");
+    print_arr(data, ARRAY_SIZE(data));
+
+    insertion_sort(data, out, ARRAY_SIZE(data));
+
+    printf("After ");
+    print_arr(out, ARRAY_SIZE(out));
+}
