40aebe91520b5ad2897d3d856844d1c494d325c5
Insub Song <sis@skaichips.co.kr>
2023-07-26 13:11:22 +0900

    [cheatsheet] update scanner.py

--- cheatsheet/scanner.py
+++ cheatsheet/scanner.py
@@ -12,7 +12,7 @@ from bleak import BleakScanner
 from colorama import Fore
 
 MAX_WINDOW_LEN = 10
-CIRCLE_COLORS = ['b', 'g', 'r', 'c', 'm', 'y']
+CIRCLE_COLORS = ['r', 'g', 'b', 'y', 'm', 'c']
 
 
 class Loop():
@@ -43,7 +43,7 @@ class Beacon():
         Beacon.num += 1
 
         # for future use
-        self.circle = plt.Circle(self.xy, radius=0, alpha=0.3, color=CIRCLE_COLORS[self.nr])
+        self.circle = plt.Circle(self.xy, radius=0, alpha=0.2, zorder=2, color=CIRCLE_COLORS[self.nr])
 
     def put_rssi(self, rssi_raw):
         self.sr[len(self.sr)] = rssi_raw
@@ -75,17 +75,20 @@ class Beacon():
 
     @property
     def r(self) -> float:
-        # n = 3
-        # d = 10 ** ((self.rssi_1m - self.rssi) / (10 * n))
+        n = 3
+        d = 10 ** ((self.rssi_1m - self.rssi) / (10 * n))
+        """
         rssi = self.rssi
         if rssi > self.rssi_1m:
             d = 10 ** (rssi / self.rssi_1m)
         else:
             d = 0.9 * 7.71 ** (rssi / self.rssi_1m) + 0.11
+        """
         return d
 
     def print_info(self):
-        s = f"[{self.nr}]{self.addr} {self.coord_text}<{self.rssi_1m}dB> {Fore.GREEN}{self.name:>6}{Fore.RESET} »"
+        s = f"[{self.nr}]{self.addr} {self.coord_text}[{self.rssi_1m}dB]"
+        s += f" {Fore.GREEN}{self.name:>6}{Fore.RESET} »"
         s += f" RSSIs: {self.rssis.tolist()} →"
         s += f" {Fore.CYAN}{self.rssi:6.2f}dB{Fore.RESET}"
         s += f" → {Fore.RED}{self.r:.1f}m{Fore.RESET}"
@@ -155,19 +158,22 @@ class Localizer():
         return [b.y for b in self.beacons]
 
     def start_plot(self):
-        self.fig, self.ax = plt.subplots(figsize=(6, 6))
-        self.fig.suptitle("Iris Indoor Positioning System")
-        self.ax.scatter(self.xs, self.ys, marker='x', s=15, c=CIRCLE_COLORS[:self.beacon_num])
+        self.fig = plt.figure(figsize=(6, 6))
+        self.fig.suptitle("Indoor Positioning System")
+        self.ax = self.fig.add_subplot()
+        self.ax.scatter(self.xs, self.ys, marker='x', s=15, zorder=3, c=CIRCLE_COLORS[:self.beacon_num])
         for beacon in self.beacons:
             self.ax.add_artist(beacon.circle)
-        self.ax.grid()
+        self.ax.grid(zorder=-1)
         plt.ion()
 
     def update_plot(self, *_):
         if not plt.get_fignums():
             return
         for beacon in self.beacons:
-            beacon.circle.set_radius(beacon.r / 10)
+            beacon.circle.set_radius(beacon.r)
+            beacon.circle.set_label(f"{beacon.name} : {beacon.r:3.1f}m")
+            self.ax.legend(frameon=False)
         plt.draw()
         plt.pause(0.1)
 
@@ -178,14 +184,14 @@ class Localizer():
 
 
 _beacons = [
-    # Beacon('D0:15:CE:BB:7A:98', x=0, y=0, rssi_1m=-50, name='sis'),
-    # Beacon('FB:D5:7C:CA:BD:CE', x=10, y=0, rssi_1m=-50, name='iron'),
-    # Beacon('F7:E7:CD:96:B5:6B', x=0, y=10, rssi_1m=-50, name='table'),
-    # Beacon('78:46:7D:00:C2:1F', x=10, y=10, rssi_1m=-46, name='iris'),
-    Beacon('78:46:7D:00:C1:C2', x=0, y=0, rssi_1m=-58, name='iris_31'),
-    Beacon('78:46:7D:00:C0:5A', x=5, y=0, rssi_1m=-58, name='iris_34'),
-    Beacon('78:46:7D:00:BF:14', x=0, y=5, rssi_1m=-58, name='iris_35'),
-    Beacon('78:46:7D:00:C0:7B', x=5, y=5, rssi_1m=-58, name='iris_36'),
+    Beacon('D0:15:CE:BB:7A:98', x=0, y=0, rssi_1m=-60, name='sis'),
+    Beacon('FB:D5:7C:CA:BD:CE', x=5, y=0, rssi_1m=-60, name='iron'),
+    Beacon('F7:E7:CD:96:B5:6B', x=0, y=5, rssi_1m=-60, name='table'),
+    Beacon('78:46:7D:00:C2:1F', x=5, y=5, rssi_1m=-60, name='iris'),
+    # Beacon('78:46:7D:00:C1:C2', x=0, y=0, rssi_1m=-58, name='iris_31'),
+    # Beacon('78:46:7D:00:C0:5A', x=5, y=0, rssi_1m=-58, name='iris_34'),
+    # Beacon('78:46:7D:00:BF:14', x=0, y=5, rssi_1m=-58, name='iris_35'),
+    # Beacon('78:46:7D:00:C0:7B', x=5, y=5, rssi_1m=-58, name='iris_36'),
 ]
 localizer = Localizer(_beacons)
 
